3955dc9e17e31dc2a68e77a2cb35579c88fda823
Samuel do nascimento ramos <hmsrp.ramos7@gmail.com>
2024-10-14 17:09:01 -0300

    Add files via upload

---
+++ script9.js
@@ -0,0 +1,14 @@
+let N1 = parseFloat(prompt("Digite a primeira nota:"))
+    let N2 = parseFloat(prompt("Digite a segunda nota:"))
+    let N3 = parseFloat(prompt("Digite a terceira nota:"))
+    let P1 = 2
+    let P2 = 3
+    let P3 = 5
+    let mediaFinal = (N1 * P1 + N2 * P2 + N3 * P3) / (P1 + P2 + P3)
+    alert(`A média final do aluno é: ${mediaFinal.toFixed(2)}`)
+    
+
+
+
+
+
